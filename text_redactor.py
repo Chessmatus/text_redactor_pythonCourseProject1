78d2c4de8b852db98826605b32531760f65ae02f
chessmatus <einullaev.ae@phystech.edu>
2023-04-07 23:18:26 +0300

    Добавил окно найти-заменить, пока без реализации

--- text_redactor.py
+++ text_redactor.py
@@ -17,6 +17,7 @@ class Window(QMainWindow):
         self.create_menu_bar()
 
         self.search_dialog = QDialog()
+        self.search_replace_dialog = QDialog()
 
     def create_menu_bar(self):
         self.setMenuBar(self.menu_bar)
@@ -24,12 +25,17 @@ class Window(QMainWindow):
         file_menu = QMenu("&Файл", self)
         self.menu_bar.addMenu(file_menu)
 
-        file_menu.addAction("Открыть", self.action_clicked)
-        file_menu.addAction("Сохранить", self.action_clicked)
-        file_menu.addAction("Найти", self.action_clicked)
+        find_menu = QMenu("&Найти", self)
+        self.menu_bar.addMenu(find_menu)
+
+        file_menu.addAction("Открыть", self.file_menu_action_clicked)
+        file_menu.addAction("Сохранить", self.file_menu_action_clicked)
+
+        find_menu.addAction("Найти", self.find_menu_action_clicked)
+        find_menu.addAction("Найти и заменить", self.find_menu_action_clicked)
 
     @QtCore.pyqtSlot()
-    def action_clicked(self):
+    def file_menu_action_clicked(self):
         action = self.sender()
         if action.text() == "Открыть":
             f_name = QFileDialog.getOpenFileName(self, "Открыть файл", "/home",
@@ -51,7 +57,10 @@ class Window(QMainWindow):
             except FileNotFoundError:
                 print("No such file")
 
-        elif action.text() == "Найти":
+    @QtCore.pyqtSlot()
+    def find_menu_action_clicked(self):
+        action = self.sender()
+        if action.text() == "Найти":
             self.search_dialog.setWindowTitle("Найти")
             self.search_dialog.resize(260, 35)
             self.search_dialog.line_edit = QtWidgets.QLineEdit(self.search_dialog)
@@ -61,6 +70,39 @@ class Window(QMainWindow):
             self.search_dialog.find_btn.clicked.connect(self.find_btn_clicked)
 
             self.search_dialog.exec_()
+
+        elif action.text() == "Найти и заменить":
+            self.search_replace_dialog.setWindowTitle("Найти и заменить")
+            self.search_replace_dialog.resize(450, 175)
+
+            self.search_replace_dialog.search_label = QtWidgets.QLabel("Найти", self.search_replace_dialog)
+            self.search_replace_dialog.search_label.move(55, 25)
+            self.search_replace_dialog.search_label.resize(35, 35)
+            self.search_replace_dialog.search_label.adjustSize()
+
+            self.search_replace_dialog.line_edit_1 = QtWidgets.QLineEdit(self.search_replace_dialog)
+            self.search_replace_dialog.line_edit_1.setGeometry(115, 15, 300, 35)
+
+            self.search_replace_dialog.replace_label = QtWidgets.QLabel("Заменить на", self.search_replace_dialog)
+            self.search_replace_dialog.replace_label.move(10, 80)
+            self.search_replace_dialog.replace_label.resize(35, 35)
+            self.search_replace_dialog.replace_label.adjustSize()
+
+            self.search_replace_dialog.line_edit_2 = QtWidgets.QLineEdit(self.search_replace_dialog)
+            self.search_replace_dialog.line_edit_2.setGeometry(115, 70, 300, 35)
+
+            self.search_replace_dialog.search_btn = QtWidgets.QPushButton("Найти", self.search_replace_dialog)
+            self.search_replace_dialog.search_btn.setGeometry(365, 125, 50, 35)
+
+            self.search_replace_dialog.replace_btn = QtWidgets.QPushButton("Заменить", self.search_replace_dialog)
+            self.search_replace_dialog.replace_btn.setGeometry(280, 125, 75, 35)
+
+            self.search_replace_dialog.replace_all_btn = QtWidgets.QPushButton("Заменить все",
+                                                                               self.search_replace_dialog)
+            self.search_replace_dialog.replace_all_btn.setGeometry(165, 125, 105, 35)
+
+            self.search_replace_dialog.exec_()
+
 
     def find_btn_clicked(self):
         words = self.search_dialog.line_edit.text()
